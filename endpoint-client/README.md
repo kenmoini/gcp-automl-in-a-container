fe0bb2bcb1193fb0181c3a7e2d7064cfa327ffef
Ken Moini <ken@kenmoini.com>
2022-04-17 20:24:00 -0400

    endpoint client works

--- endpoint-client/README.md
+++ endpoint-client/README.md
@@ -9,6 +9,42 @@ export GCP_PROJECT_ID=<your GCP project ID>
 export GCP_AI_ENDPOINT_ID=<your GCP AI Endpoint ID>
 export GCP_LOCATION=<your GCP region>
 
-## Run the client
-python3 main.py
+## Run the client with a single image
+python3 main.py -f <path to image file>
+
+## Run the client as a server middleware
+python3 main.py -m server -p <port>
+```
+
+## Run as a Container
+
+### Build the Container
+
+```bash
+sudo podman build -t automl-endpoint-client .
+```
+
+### Running the Container
+
+#### Running as a Server
+
+```bash
+sudo podman run --name automl-endpoint-client --rm \
+ -v /opt/gcp-service-account-file.json:/opt/gcp-service-account-file.json:Z \
+ -e GOOGLE_APPLICATION_CREDENTIALS="/opt/gcp-service-account-file.json" \
+ -e GCP_PROJECT_ID=<your GCP project ID> \
+ -e GCP_AI_ENDPOINT_ID=<your GCP AI Endpoint ID> \
+ -p 8080:8080 automl-endpoint-client
+```
+
+#### Running as a Single-Shot Job
+
+```bash
+sudo podman run --name automl-endpoint-client --rm -it \
+ -v /opt/gcp-service-account-file.json:/opt/gcp-service-account-file.json:Z \
+ -v ./test2.jpg:/opt/app-src/test.jpg:Z \
+ -e GOOGLE_APPLICATION_CREDENTIALS="/opt/gcp-service-account-file.json" \
+ -e GCP_PROJECT_ID=<your GCP project ID> \
+ -e GCP_AI_ENDPOINT_ID=<your GCP AI Endpoint ID> \
+ automl-endpoint-client python3 /opt/app-src/main.py -f /opt/app-src/test.jpg
 ```
--- endpoint-client/main.py
+++ endpoint-client/main.py
@@ -1,10 +1,20 @@
 # [START aiplatform_predict_image_classification_sample]
 import base64
 import os
+import sys, getopt
+import time
+import json
+import proto
+import string
+
 
 from google.cloud import aiplatform
 from google.cloud.aiplatform.gapic.schema import predict
 from google.oauth2.service_account import Credentials
+from google.protobuf.json_format import MessageToJson
+from flask_cors import CORS, cross_origin
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
 
 # Get environs
 homePath = "/root" if os.getenv("HOME") is None else os.getenv("HOME")
@@ -16,7 +26,9 @@ apiEndpoint = (gcpLocation + "-aiplatform.googleapis.com") if os.getenv('GCP_API
 
 credentials = Credentials.from_service_account_file(gcpSACredentialsJSONFile)
 
-def predict_image_classification_sample(
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def predict_image_classification(
     project: str,
     endpoint_id: str,
     filename: str,
@@ -55,19 +67,124 @@ def predict_image_classification_sample(
     response = client.predict(
         endpoint=endpoint, instances=instances, parameters=parameters
     )
+    #print("response")
+    #print(" deployed_model_id:", response.deployed_model_id)
+    # See gs://google-cloud-aiplatform/schema/predict/prediction/image_classification_1.0.0.yaml for the format of the predictions.
+    return response
+    #predictions = response.predictions
+    #for prediction in predictions:
+    #    #print(" prediction:", dict(prediction))
+    #    print(dict(prediction))
+
+def predict_image_classification_sample(response):
     print("response")
     print(" deployed_model_id:", response.deployed_model_id)
-    # See gs://google-cloud-aiplatform/schema/predict/prediction/image_classification_1.0.0.yaml for the format of the predictions.
     predictions = response.predictions
     for prediction in predictions:
         print(" prediction:", dict(prediction))
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def main(argv):
+    mode = ''
+    inputfile = ''
+    port = '8080'
+    try:
+        opts, args = getopt.getopt(argv,"hm:f:p:",["mode=","file=","port="])
+    except getopt.GetoptError:
+        print ('main.py [-m server] [-p <port>] -f <inputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+      if opt == '-h':
+          print ('main.py [-m server] [-p <port>] -f <inputfile>')
+          sys.exit()
+      elif opt in ("-m", "--mode"):
+          mode = arg.strip()
+      elif opt in ("-f", "--file"):
+          inputfile = arg.strip()
+      elif opt in ("-p", "--port"):
+          port = arg.strip()
+    print ('Mode is', mode)
+    print ('Input file is', inputfile)
+    print ('Port is', port)
+
+    if mode == 'server':
+        ## Run an HTTP Server to respond to ase64 encoded images
+        print ("Running in server mode")
+        
+        ## Create a flask server and listen on port 8080
+        app = Flask(__name__)
+        app.config['UPLOAD_FOLDER'] = '/tmp'
+        # After creating the Flask app, you can make all APIs allow cross-origin access.
+        CORS(app)
+        # or a specific API
+        @app.route("/predict", methods=['POST'])
+        @cross_origin()
+        def upload_file():
+            print ("Received a POST predict request")
+            if request.method == 'POST':
+                # check if the post request has the file part
+                if 'file' not in request.files:
+                    print ('No file part')
+                    return 'No file part'
+                file = request.files['file']
+                # if user does not select file, browser also
+                # submit a empty part without filename
+                if file.filename == '':
+                    print ('No selected file')
+                    return 'No selected file'
+                if file and allowed_file(file.filename):
+                    now = int( time.time() )
+                    filename = str(now) + secure_filename(file.filename)
+                    print ("Filename: ", filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    # Call predict_image_classification
+                    predictionCall = predict_image_classification(
+                        project=projectID,
+                        endpoint_id=endpointID,
+                        filename=os.path.join(app.config['UPLOAD_FOLDER'], filename),
+                        location=gcpLocation,
+                        api_endpoint=apiEndpoint,
+                    )
+                    
+                    predictionJSON = {"deployed_model_id": predictionCall.deployed_model_id, "model_name": predictionCall.model_display_name,  "data": []}
+
+                    predictions = predictionCall.predictions
+                    for prediction in predictions:
+                        d = dict(prediction)
+                        objJSON = {"id": d['ids'][0], "confidences": d['confidences'][0], "display_names": d['displayNames'][0]}
+                        predictionJSON["data"].append(objJSON)
+
+                    jsonObj = {
+                        "project_id": projectID,
+                        "endpoint_id": endpointID,
+                        "filename": os.path.join(app.config['UPLOAD_FOLDER'], filename),
+                        "location": gcpLocation,
+                        "api_endpoint": apiEndpoint,
+                        "prediction": predictionJSON,
+                    }
+
+                    return jsonObj
+        app.run(host='0.0.0.0', port=port)
+            
+
+    else:
+        filename = inputfile if inputfile is not '' else "test.jpg"
+        predict_image_classification_sample(predict_image_classification(
+            project=projectID,
+            endpoint_id=endpointID,
+            location=gcpLocation,
+            api_endpoint=apiEndpoint,
+            filename=filename
+        ))
+
 
 # [END aiplatform_predict_image_classification_sample]
-predict_image_classification_sample(
-    project=projectID,
-    endpoint_id=endpointID,
-    location=gcpLocation,
-    api_endpoint=apiEndpoint,
-    filename="test.jpg"
-)
+
+# [FROM https://www.tutorialspoint.com/python/python_command_line_arguments.htm]
+
+## Run the main application
+if __name__ == "__main__":
+    main(sys.argv[1:])
